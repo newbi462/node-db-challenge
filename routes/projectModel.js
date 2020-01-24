2bf8fc443e169c9388dba6f2a1d95df7502117d6
newbi462 <isthisemailfree@gmail.com>
2020-01-24 12:29:35 -0500

    adding resources.

--- routes/projectModel.js
+++ routes/projectModel.js
@@ -1,5 +1,21 @@
 const db = require('../data/db.js');
 
 module.exports = {
-  //
+  addingResources,
 }
+
+function addingResources(reqObject) {
+  return db("resource").insert(reqObject);
+    /*.then(idOfAdded => {
+      console.log(idOfAdded);
+      db("resource").where({id:idOfAdded[0]})
+    });*/
+}
+
+/*
+adding resources.
+
+when adding resources the client must provide a name, the description is optional.
+
+
+*/
--- routes/projectRouter.js
+++ routes/projectRouter.js
@@ -4,5 +4,18 @@ const ProjectModel = require('./projectModel.js');
 
 const router = express.Router();
 
+router.post('/addres', (request, responce) => {
+  //db("cars").insert(request.body)
+  ProjectModel.addingResources(request.body)
+    .then(addedID => {
+      console.log(addedID);
+      responce.json(addedID);
+    })
+    .catch( error => {
+      console.log(error);
+      responce.status(500).json( {error: "Post Failed."} )
+    })
+});
+
 
 module.exports = router;
